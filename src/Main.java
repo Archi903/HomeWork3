a38b35dad36668bffe4e84ffa92508d42239875c
Archi903 <artpele@yandex.ru>
2022-11-30 18:06:08 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        task1 ();
+        task1();
+        task2();
     }
-    public static void task1 () {
+
+    public static void task1() {
         System.out.println("Задание 1");
         byte Sheriff = 1;
         System.out.println("Значение переменной Sheriff с типом byte равно " + Sheriff);
@@ -16,7 +18,27 @@ public class Main {
         System.out.println("Значение переменной Mars с типом float равно " + Mars);
         double Sun = 10;
         double SunShine = Sun / 3;
-System.out.println("Значение переменной SunShine с типом double равно " + SunShine);
+        System.out.println("Значение переменной SunShine с типом double равно " + SunShine);
     }
 
+    public static void task2() {
+        System.out.println("Задание 2");
+
+        float A = 27.12f;
+        System.out.println(A + " принадлежит float" );
+        long B = 987678965549L;
+        System.out.println(B + " принадлежит long" );
+        float C = 2.786f;
+        System.out.println(C + " принадлежит float" );
+        boolean D = C >= 2;
+        System.out.println(D + " принадлежит boolean" );
+        short E = 569;
+        System.out.println(E + " принадлежит short" );
+        short G = -159;
+        System.out.println(G + " принадлежит short" );
+        short F = 27897;
+        System.out.println(F + " принадлежит short" );
+        byte J = 67;
+        System.out.println(J + " принадлежит byte" );
+    }
 }
