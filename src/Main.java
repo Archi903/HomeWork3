0c2337075097d2236af6488c777b955e540e41cb
Archi903 <artpele@yandex.ru>
2022-11-30 17:35:31 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1 ();
+    }
+    public static void task1 () {
+        System.out.println("Задание 1");
+        byte Sheriff = 1;
+        System.out.println("Значение переменной Sheriff с типом byte равно " + Sheriff);
+        short ants = 32000;
+        System.out.println("Значение переменной ants с типом short равно " + ants);
+        int redHats = 35000;
+        System.out.println("Значение переменной redHats с типом int равно " + redHats);
+        long brokenLamps = 30000000000L;
+        System.out.println("Значение переменной brokenLamps с типом long равно " + brokenLamps);
+        float Mars = 3.6545f;
+        System.out.println("Значение переменной Mars с типом float равно " + Mars);
+        double Sun = 10;
+        double SunShine = Sun / 3;
+System.out.println("Значение переменной SunShine с типом double равно " + SunShine);
     }
 }
